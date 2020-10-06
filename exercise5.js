ab853fbeb37623a0223ad73100206a2a1c1c819f
Andrey Sazonov <61043163+firefighter-19@users.noreply.github.com>
2020-10-06 11:31:46 +0300

    added exercise

--- exercise5.js
+++ exercise5.js
@@ -6,7 +6,3 @@ if ((this.age >= 18 && this.age <= 35) && (this.paid === true && this.blocked ==
 	console.log('Access allowed');
 }
 console.log('Access denied');
-
-//Реализовать if для проверки доступа (через тернарный оператор)
-
-let accessAllowed2 = ((this.age >= 18 && this.age <= 35) && (this.paid === true && this.blocked === false && this.badUsername === false || this.isAdmin === true)) ? console.log('Access allowed') : console.log('Access denied');
