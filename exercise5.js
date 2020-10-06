101fb2baaeb4cf74324ed7edda6d07329ab5f85a
Andrey Sazonov <61043163+firefighter-19@users.noreply.github.com>
2020-10-06 20:59:49 +0300

    Delete exercise5.js

--- exercise5.js
+++
@@ -1,7 +0,0 @@
-// Реализовать if для проверки доступа
-
-let accessAllowed;
-if ((this.age >= 18 && this.age <= 35) && (this.paid === true && this.blocked === false && this.badUsername === false || this.isAdmin === true)) {
-	console.log('Access allowed');
-}
-console.log('Access denied');
