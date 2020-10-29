c1a6342036f262a1d166b1f7c70211871f665f9d
Andrey Sazonov <61043163+firefighter-19@users.noreply.github.com>
2020-10-29 22:20:50 +0300

    Вопрос с калькулятором решен.
    Со второй задачей все также не могу найти решения.

--- 3. Функции/calculator.js
+++ 3. Функции/calculator.js
@@ -1,9 +1,28 @@
-function Calculator() {
-    this.add = function() {
-        return this.a + this.b
-    },
-    this.substract = function() {
-        return this.a - this.b
-    },
+function Calc() {
+    this.methods = {
+      };
+    
+    this.operation = function (str) {
+
+        let split = str.split(' '),
+        a = +split[0],
+        op = split[1],
+        b = +split[2]
+
+    if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+            return NaN;
+        }
+        return this.methods[op](a, b);
+    };
+
+    this.addOperation = function(name, func) {
+        this.methods[name] = func;
+      };
 }
 
+let calculator = new Calc();
+
+calculator.addOperation('+', (a, b) => a + b)
+console.log(calculator.operation('1 + 2'))
+calculator.addOperation('/', (a, b) => a / b)
+console.log(calculator.operation('2 / 2'))
