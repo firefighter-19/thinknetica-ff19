9f934d7e220c01e41044277becb0b379de59a9ab
Andrey Sazonov <61043163+firefighter-19@users.noreply.github.com>
2020-11-03 20:28:26 +0300

    Delete exercise2-task3.js

--- exercise2-task3.js
+++
@@ -1,8 +0,0 @@
-//Реализовать функцию для фильтрации массива по длине слов
-
-const fruits = ['orange', 'apple', 'banana', '']
-
-const filterByLength = (array, zero, length) => array.filter(el => el.length <= length || el.length === zero)
-
-console.log(filterByLength(fruits, 0, 5)) // [‘apple’, ‘’]
-
