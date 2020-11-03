398b02394028e820b77ca1025dce41f9b36c384a
Andrey Sazonov <61043163+firefighter-19@users.noreply.github.com>
2020-11-03 20:27:57 +0300

    Delete exercise2-task1.js

--- exercise2-task1.js
+++
@@ -1,8 +0,0 @@
-//Реализовать функцию, которая принимает текст, и возвращает массив объектов со структурой
-const isCapitalized = str => str[0] === str[0].toUpperCase()
-
-let string = 'Я люблю теслу';
-let newArray = string.split(' ').map((word) => ({ word: word, length: word.length, isCapitalized: isCapitalized(word) }))
-console.log(newArray)
-
-
