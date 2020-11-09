42a358edd2575fdab065e83d51816938e2d94252
Andrey Sazonov <61043163+firefighter-19@users.noreply.github.com>
2020-11-09 22:39:41 +0300

    incapsulated? (Hope it's correct word)
    1.incapsulated variable to the constructor Shipyard;
    
    2. fixed generateRangomID func.

--- 4. Прототипы и наследование/ships.js
+++ 4. Прототипы и наследование/ships.js
@@ -1,11 +1,10 @@
 function getRandomID(min = 0, max = 100) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    let result = '';
     // if (ships[number] === result) {
     //     return `This number is already used`
     // }
-    result = Math.floor(Math.random() * (max - min)) + min;
+    let result = Math.floor(Math.random() * (max - min)) + min;
     return result.toString()
 };
 
@@ -29,7 +28,7 @@ function EnginedShipyard() {
     this.type = 'engined';
     this.buildEngineShip = function(hp, body) {
         let number = getRandomID();
-        ships[number] = new EnginedShip(hp, body);
+        this.ships[number] = new EnginedShip(hp, body);
         return `You've succefully built engined ship number ${number}`
     }
 }
@@ -38,44 +37,45 @@ function SailingShipyard() {
     this.type = 'sail';
     this.buildSailShip = function(mastNum, mastArea) {
         let number = getRandomID();
-        ships[number] = new SailingShip(mastNum, mastArea);
+        this.ships[number] = new SailingShip(mastNum, mastArea);
         return `You've succefully built sail ship number ${number}`
     }
 }
 
 function Shipyard() {
+    let ships = {
+        '23': new EnginedShip(50, 'wooden'),
+        '17': new SailingShip(2, 100)
+    }
+    
+    this.ships = ships;
+
     this.getInfo = function() {
         return ships
     },
     this.changeColor = function(number, color) {
         if (ships[number] !== undefined) {
-            ships[number]['color'] = color
+            this.ships[number]['color'] = color
             return `You've succefully painted number ${number} ship`
         }
     },
     this.repair = function(number) {
-        if (ships[number]['type'] === this.type && ships[number] != undefined) {
-            ships[number]['lastRepair'] = new Date().toLocaleDateString();
-            return `You've succefully repaired ${ships[number].type} ship number ${number}`
+        if (this.ships[number]['type'] === this.type && this.ships[number] != undefined) {
+            this.ships[number]['lastRepair'] = new Date().toLocaleDateString();
+            return `You've succefully repaired ${this.ships[number].type} ship number ${number}`
         }
     },
     this.replace = function(number) {
-        if (ships[number]['type'] === this.type && ships[number] != undefined) {
+        if (this.ships[number]['type'] === this.type && this.ships[number] != undefined) {
             let newNumber = getRandomID();
-            ships[newNumber] = ships[number];
-            ships[newNumber]['lastRepair'] = null;
+            this.ships[newNumber] = ships[number];
+            this.ships[newNumber]['lastRepair'] = null;
             delete ships[number]
-            return `You've succefully replaced ${ships[newNumber].type} ship number ${number}`
+            return `You've succefully replaced ${this.ships[newNumber].type} ship number ${number}`
         }
     }
 
 }
-
-let ships = {
-    '23': new EnginedShip(50, 'wooden'),
-    '17': new SailingShip(2, 100)
-};
-
 
 EnginedShipyard.prototype = new Shipyard();
 SailingShipyard.prototype = new Shipyard();
