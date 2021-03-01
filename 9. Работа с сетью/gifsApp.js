4d4285b96cde3f0d97db4df25f8b5fc2a65bb0f2
Andrey Sazonov <61043163+firefighter-19@users.noreply.github.com>
2021-03-01 10:13:20 +0300

    cache problem fixed (used Map)

--- 9. Работа с сетью/gifsApp.js
+++ 9. Работа с сетью/gifsApp.js
@@ -4,7 +4,7 @@ let iframe = document.querySelector('iframe');
 const getGif = () => {
 	const apiGiphy = 'qjL5hFaEXiaEXz7atrAFipTLjqUwbllH';
 	input.addEventListener('input', event => event.target.value)
-	let name = input.value
+	let name = input.value;
 	return `https://api.giphy.com/v1/gifs/search?q=${name}&api_key=${apiGiphy}&limit=1`
 }
 
@@ -36,10 +36,15 @@ const apiCall = (url) => {
 }
 
 const checkCache = () => {
-	let gifsCache;
-	return function (data) {
+	let gifsCache = new Map();
+	return function (name, data) {
 		for (let key of data) {
-			return gifsCache = key.embed_url;
+			if (!gifsCache.has(name)) {
+				gifsCache.set(search = name, link = key.embed_url);
+				return gifsCache.get(name)
+			} else {
+				return gifsCache.get(name)
+			}
 		}
 	}
 }
@@ -48,9 +53,9 @@ let cache = checkCache();
 input.addEventListener('keydown', () => {
 	setTimeout(() => {
 		apiCall(getGif())
-			.then(gifs => cache(gifs.data))
+			.then(gifs => cache(input.value, gifs.data))
 			.then(gifs => {
 				iframe.setAttribute('src', gifs)
 			})
-	}, 5000);
+	}, 1500);
 })
