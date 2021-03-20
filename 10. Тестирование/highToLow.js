3e2a9ed18819f15e71817bdc2b2a1c84af828eae
Andrey Sazonov <61043163+firefighter-19@users.noreply.github.com>
2021-03-20 15:48:51 +0300

    added tests for the highToLow.js

--- 10. Тестирование/highToLow.js
+++ 10. Тестирование/highToLow.js
@@ -56,33 +56,97 @@ class Calculator {
 	addOperation(command, func) {
 		this.methods[command] = func;
 	}
-
 	performOperation(operation) {
-		const [operator1, operand, operator2] = operation.split(' ');
-		this.history.push(operation)
-		return this.methods[operand](+operator1, +operator2);
+		let [operator1, operand, operator2] = operation.split(' ');
+		if (!this.methods[operand] || isNaN(operator1) || isNaN(operator2)) {
+			return NaN
+		} else {
+			this.history.push(operation)
+			return this.methods[operand](+operator1, +operator2);
+		}
 	}
-
 	showOperations() {
 		return this.methods;
 	}
 	showHistory() {
 		return this.history;
 	}
+	clearHistory() {
+		this.history = [];
+		return 'history is clear'
+	}
 }
+
+let calculator = new Calculator;
 
 const addition = (a, b) => a + b;
 const substraction = (a, b) => a - b;
 const multiplication = (a, b) => a * b;
 const division = (a, b) => a / b;
 
-let calculator = new Calculator;
-
 calculator.addOperation('+', addition)
 calculator.addOperation('-', substraction)
-console.log(calculator.performOperation('2 + 1'))
-console.log(calculator.performOperation('2 + 4'))
+calculator.addOperation('*', multiplication)
+calculator.addOperation('/', division)
 
-console.log(calculator.showOperations())
-console.log(calculator.showHistory())
+//=================================================ticketSales
 
+class TicketSales {
+	constructor() {
+		this.events = {};
+		this.storage = 0;
+		this.tickets = {
+			'123456': 300,
+		};
+		this.id = (min = 500000, max = 999999) => {
+			min = Math.ceil(min);
+			max = Math.floor(max);
+			let generatedID = Math.floor(Math.random() * (max - min)) + min;
+			return generatedID;
+		};
+	};
+	getEvents() {
+		return this.events;
+	};
+	getMoneyStorage() {
+		return this.storage;
+	};
+	showTickets() {
+		return this.tickets;
+	};
+	createEvent(name, cost) {
+		if (typeof (name) !== 'string') {
+			return 'Something went wrong'
+		}
+		this.events[name] = cost;
+		return `You've created event ${name} which costs ${cost} `
+	};
+	buyTicket(name) {
+		if (this.events.hasOwnProperty(name)) {
+			this.storage += this.events[name];
+			let id = this.id();
+			this.tickets[id] = this.events[name];
+			return `You've bought a ticket id ${id}`
+		} else {
+			return `There is no such event`
+		}
+	};
+	returnTicket(id) {
+		this.storage -= this.tickets[id];
+		delete this.tickets[id];
+		return `You've returned a ticket for ${id}`;
+	}
+}
+
+let ticketWindow = new TicketSales;
+
+// console.log(ticketWindow.createEvent('Mad', 300));
+// console.log(ticketWindow.createEvent('Good', 300));
+// console.log(ticketWindow.buyTicket('Mad'));
+// console.log(ticketWindow.buyTicket('Good'));
+// console.log(ticketWindow.getEvents());
+// console.log(ticketWindow.getMoneyStorage());
+// console.log(ticketWindow.showTickets());
+// console.log(ticketWindow.returnTicket('123456'));
+// console.log(ticketWindow.getMoneyStorage());
+// console.log(ticketWindow.showTickets());
--- 10. Тестирование/highToLow.test.js
+++ 10. Тестирование/highToLow.test.js
@@ -1,3 +1,5 @@
+//=================================================word.length
+
 describe('Filter array from high to low', () => {
 	it('Should return array as a result', () => assert.isArray(data));
 	it('Should return from high to low', () => assert.deepEqual(fromHighToLow(data), [30, 20, 10, 0, -10, -20]));
@@ -10,6 +12,8 @@ describe('Filter array by word length', () => {
 	});
 })
 
+//=================================================caching
+
 describe('Cache', () => {
 	it('Should return the same result twice', () => {
 		assert.deepEqual(calculate(3, 3), { value: 27, fromCache: false })
@@ -20,20 +24,84 @@ describe('Cache', () => {
 	});
 })
 
+//=================================================moneyBox
+
 describe('MoneyBox', () => {
 	it('Should add coin', () => {
-		assert.equal(moneyBox.addCoin())
+		assert.equal(moneyBox.getAmount(), 0)
+		moneyBox.addCoin();
+		assert.equal(moneyBox.getAmount(), 1)
 	});
 	it('Should return amount', () => {
 		assert.equal(moneyBox.getAmount(), 1)
 	});
 })
 
-describe('MoneyBox', () => {
-	it('Should add coin', () => {
-		assert.equal(moneyBox.addCoin())
+//=================================================calculator
+
+describe('Calculator', () => {
+	it('Should add an operation', () => {
+		const operation = () => { };
+		assert.equal(calculator.methods['test'], undefined);
+		calculator.addOperation('test', operation);
+		assert.equal(calculator.methods['test'], operation);
+	})
+	it('Should show history', () => {
+		assert.deepEqual(calculator.showHistory(), [])
+	})
+	it('Should clear history', () => {
+		assert.equal(calculator.clearHistory(), 'history is clear')
+	})
+	describe('Math functions', () => {
+		describe('Addition', () => {
+			it('Should perform an operation', () => {
+				assert.equal(addition(2, 2), 4)
+			})
+		});
+		describe('Substraction', () => {
+			it('Should perform an operation', () => {
+				assert.equal(substraction(4, 2), 2)
+			})
+		});
+		describe('Multiplication', () => {
+			it('Should perform an operation', () => {
+				assert.equal(multiplication(2, 3), 6)
+			})
+		});
+		describe('Division', () => {
+			it('Should perform an operation', () => {
+				assert.equal(division(4, 2), 2)
+			})
+		});
 	});
-	it('Should return amount', () => {
-		assert.equal(moneyBox.getAmount(), 1)
-	});
+});
+
+//=================================================ticketSales
+
+describe('Ticket sales', () => {
+	it('Should show the events', () => {
+		assert.equal(ticketWindow.events['Saw'], undefined)
+		ticketWindow.createEvent('Saw', 300);
+		assert.equal(ticketWindow.events['Saw'], 300)
+	})
+	it('Should show money storage', () => {
+		assert.equal(ticketWindow.storage, 0)
+	})
+	it('Should show the tickets', () => {
+		assert.deepEqual(ticketWindow.tickets, { '123456': 300 })
+	})
+	it('Should create id for the tickets', () => {
+		let result = ticketWindow.id;
+		assert.isNumber(ticketWindow.id(), console.log(result))
+	})
+	it('Should return a ticket', () => {
+		assert.deepEqual(ticketWindow.tickets, { '123456': 300 })
+		ticketWindow.returnTicket('123456')
+		assert.deepEqual(ticketWindow.showTickets(), {})
+	})
+	it('Should buy a ticket', () => {
+		assert.deepEqual(ticketWindow.showTickets(), {})
+		ticketWindow.buyTicket('Saw')
+		ticketWindow.showTickets();
+	})
 })
