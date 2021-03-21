b9e49407c5e9c8bd86f7c98ace3b094a0b1a56c7
Andrey Sazonov <61043163+firefighter-19@users.noreply.github.com>
2021-03-21 13:00:43 +0300

    x

--- 3. Функции/calculator.js
+++ 3. Функции/calculator.js
@@ -1,46 +1,52 @@
 function Calc() {
 
-    this.methods = {
-    };
+	this.methods = {
+	};
 
-    let operations = [];
+	let operations = [];
 
-    this.history = function() {
-        return operations;
-    }
+	this.history = function () {
+		return operations;
+	}
 
-    this.clearHistory = function() {
-        operations = [];
-        return operations;
-    }
+	this.clearHistory = function () {
+		operations = [];
+		return operations;
+	}
 
-    this.operation = function (str) {
+	this.operation = function (str) {
 
-        let split = str.split(' '),
-        a = +split[0],
-        op = split[1],
-        b = +split[2]
-        
-    if (!this.methods[op] || isNaN(a) || isNaN(b)) {
-            return NaN;
-        }
-        operations.push({operation: op, operands: a,b})
-        return this.methods[op](a,b);
+		let split = str.split(' '),
+			a = +split[0],
+			op = split[1],
+			b = +split[2]
 
-    };
+		if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+			return NaN;
+		}
+		operations.push({ operation: op, operands: a, b })
+		return this.methods[op](a, b);
 
-    this.addOperation = function(name, func) {
-        this.methods[name] = func;
-    };
+	};
+
+	this.addOperation = function (name, func) {
+		this.methods[name] = func;
+	};
 }
+
+const addition = (a, b) => a + b;
+const substraction = (a, b) => a - b;
+const multiplication = (a, b) => a * b;
+const division = (a, b) => a / b;
 
 let calculator = new Calc();
 
-calculator.addOperation('+', (a, b) => a + b)
-calculator.addOperation('/', (a, b) => a / b)
-console.log(calculator.operation('2 / 2'))
-calculator.operation('1 + 2');
 
+calculator.addOperation('+', addition)
+calculator.addOperation('-', substraction)
+calculator.addOperation('*', multiplication)
+console.log(calculator.operation('2 + 2'))
+console.log(calculator.methods)
 console.log(calculator.history())
 console.log(calculator.clearHistory())
 
