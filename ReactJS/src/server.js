2d55bed6bc77a9c13820eb78d9f30feaa9774f40
Andrey Sazonov <61043163+firefighter-19@users.noreply.github.com>
2021-04-25 16:51:21 +0300

    added commit for server.js

--- ReactJS/src/server.js
+++ ReactJS/src/server.js
@@ -5,27 +5,26 @@ const app = express();
 
 const { render } = require('./render');
 
-console.log(render())
+// for start write "node ./src/server.js"
+app.get('/', (req, res) => {
+	res.send(`
+		<!DOCTYPE html>
+		<html lang="en">
 
-// app.get('/', (req, res) => {
-// 	res.send(`
-// 		<!DOCTYPE html>
-// 		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Document</title>
+		</head>
 
-// 		<head>
-// 			<meta charset="UTF-8">
-// 			<meta http-equiv="X-UA-Compatible" content="IE=edge">
-// 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-// 			<title>Document</title>
-// 		</head>
+		<body>
+			<div id="root">${render()}</div>
+			<script src="index.js"></script>
+		</body>
 
-// 		<body>
-// 			<div id="root">${render()}</div>
-// 			<script src="index.js"></script>
-// 		</body>
+		</html>
+	`)
+})
 
-// 		</html>
-// 	`)
-// })
-
-// app.listen(3000, () => console.log('Server is ready, port 3000 is opened'))
+app.listen(3000, () => console.log('Server is ready, port 3000 is opened'))
